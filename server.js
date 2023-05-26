f093d4314b9936382b189f77a43d69f5fad11b4d
eh-lee <fornewstack@daum.net>
2023-05-26 18:35:39 +0900

    [Feat] MongoDB connected with mongoose commit

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const PORT = 3000;
 
 const app = express();
 const productRoutes = require("./routes/products")
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://fornewstack:dmsgud32@cluster0.88jrk7e.mongodb.net/tdd?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true
+    }).then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 // endpoint가 /api/products/sth인 경우 productRoutes로 보내준다
 app.use("/api/products", productRoutes);
