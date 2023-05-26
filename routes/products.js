a366bd5af3d83796189891aa10ba6f0e649be777
eh-lee <fornewstack@daum.net>
2023-05-26 18:26:21 +0900

    [Feat] products > route, controller commit

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const productsController = require("../controller/products");
 
-router.get("/", (req, res) => {
-    res.send("Router connected")
-});
+router.get("/", productsController.try);
 
 module.exports = router;
