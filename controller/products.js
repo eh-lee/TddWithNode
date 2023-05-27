d85980585e007b424ac47a7b86a55654de6799f5
eh-lee <fornewstack@daum.net>
2023-05-27 16:57:44 +0900

    [Feat] Create Unit Test Commit

--- controller/products.js
+++ controller/products.js
@@ -1,3 +1,11 @@
-exports.try = (req, res) => {
-    res.send("Router connected")
-}
+const productModel = require("./../models/Product");
+exports.createProduct = (req, res, next) => {
+    // 그냥 create method임.
+    const sample = productModel.create(req.body);
+    res.status(201).json(sample);
+};
+
+// ======================================================
+// exports.try = (req, res) => {
+//     res.send("Router connected")
+// }
