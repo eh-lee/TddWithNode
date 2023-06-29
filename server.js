a7b9b5b2295d1b4f6828133a1e3e8b23a547f23b
eh-lee <fornewstack@daum.net>
2023-06-30 01:21:34 +0900

    [Feat] server.js commit

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-// node.js에서는 require == import
-const express = require("express");
+const express = require('express');
 
-const PORT = 3000;
+const PORT = 5000;
+// const HOST = '0.0.0.0';
 
+//App
 const app = express();
-const productRoutes = require("./routes/products")
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://fornewstack:dmsgud32@cluster0.88jrk7e.mongodb.net/tdd?retryWrites=true&w=majority",
-    {
-        useNewUrlParser: true
-    }).then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
-
-// endpoint가 /api/products/sth인 경우 productRoutes로 보내준다
-app.use("/api/products", productRoutes);
-
-app.use = (express.json());
-app.get("/", (req, res) => {
-    res.send("hello eh-lee")
+app.get('/', (req, res) => {
+    res.send("Hello World");
 });
 
+// app.listen(PORT, HOST);
 app.listen(PORT);
-console.log(`Running on PORT ${PORT}`);
+console.log(`Running on http://${HOST}:${PORT}`);
