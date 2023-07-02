d9370cb12961546b4c5d108f1116285429e3f1b4
eh-lee <fornewstack@daum.net>
2023-07-02 17:21:18 +0900

    [Fix] route.get() callback func error fixed commit

--- controller/products.js
+++ controller/products.js
@@ -3,4 +3,4 @@ const productModel = require("../models/Product");
 exports.createProduct = (req, res, next) => {
     const createdProduct = productModel.create(req.body);
     res.status(201).json(createdProduct);
-}; 
+};
--- routes.js
+++ routes.js
@@ -1,8 +1,16 @@
+// const express = require('express');
+// const router = express.Router();
+
+// //middleware 선언 && 호출
+// const productController = require("./controller/products");
+// router.get('/', productController.hello);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
-
-//middleware 선언 && 호출
 const productController = require("./controller/products");
-router.get('/', productController.hello);
+
+router.get('/', productController.createProduct);
 
 module.exports = router;
