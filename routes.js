568fbbf86397bcd97a0fa0fdcbe505dad53a191a
eh-lee <fornewstack@daum.net>
2023-07-02 17:56:38 +0900

    [Feat] test error handling commit

--- routes.js
+++ routes.js
@@ -1,16 +1,8 @@
-// const express = require('express');
-// const router = express.Router();
-
-// //middleware 선언 && 호출
-// const productController = require("./controller/products");
-// router.get('/', productController.hello);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const productController = require("./controller/products");
 
 router.get('/', productController.createProduct);
+router.post('/', productController.createProduct);
 
 module.exports = router;
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -9,7 +9,7 @@ let req, res, next;
 beforeEach(() => {
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
-    next = null;
+    next = jest.fn();
 })
 
 describe("Product Controller Create", () => {
@@ -21,36 +21,31 @@ describe("Product Controller Create", () => {
         expect(typeof productController.createProduct).toBe("function");
     });
 
-    it("should call ProductModel.create", () => {
-        productController.createProduct(req, res, next);
+    it("should call ProductModel.create", async () => {
+        await productController.createProduct(req, res, next);
         expect(productModel.create).toBeCalledWith(newProduct);
     })
 
-    it("should return 201 res code", () => {
-        productController.createProduct(req, res, next);
+    it("should return 201 res code", async () => {
+        await productController.createProduct(req, res, next);
         expect(res.statusCode).toBe(201);
         expect(res._isEndCalled()).toBeTruthy();
         // send가 있다면, send(결과값)가 잘 전달되었는지 확인
     })
 
-    it("should return json body in res", () => {
+    it("should return json body in res", async () => {
         productModel.create.mockReturnValue(newProduct);
         // mockFunc가 어떤 결과값을 반활할지 알려줄 때
-        productController.createProduct(req, res, next);
+        await productController.createProduct(req, res, next);
         expect(res._getJSONData()).toStrictEqual(newProduct);
     })
+
+    it("should handle errors", async () => {
+        const errorMessage = { messgae: "property missing" };
+        const rejectedPromise = Promise.reject(errorMessage);
+        productModel.create.mockReturnValue(rejectedPromise);
+        await productController.createProduct(req, res, next);
+        expect(next).toBeCalledWith(errorMessage);
+        // next : middleware 인자. 비동기 요청에 대한 error handling.
+    })
 });
-
-//====================================================
-//============== Ex 1) "Calculation" =================
-//====================================================
-
-// describe("Calculation", () => {
-//     test('two plus two is four', () => {
-//         expect(2 + 2).toBe(4);
-//     })
-
-//     test('two plus two is not five', () => {
-//         expect(2 + 2).not.toBe(5);
-//     })
-// })
