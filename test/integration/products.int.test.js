4e8fd5cb2a76c42fa2c977cb14a7eddbe80cac9f
eh-lee <fornewstack@daum.net>
2023-07-06 19:06:13 +0900

    [Feat] getProductById Int test commit

--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../server');
 const newProduct = require('../data/new-product.json')
+let firstProduct;
 
 // =============================================================
 // ========================== POST =============================
@@ -33,4 +34,17 @@ it("GET /api/products", async () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].name).toBeDefined();
     expect(response.body[0].description).toBeDefined();
+    firstProduct = response.body[0];
+})
+
+it("GET /api/product/:productId", async () => {
+    const response = await request(app).get('/api/products/' + firstProduct._id)
+    expect(response.statusCode).toBe(200);
+    expect(response.body.name).toBe(firstProduct.name);
+    expect(response.body.description).toBe(firstProduct.description);
+})
+
+it("GET id doesnt exist /api/product/:productId", async () => {
+    const response = await request(app).get('/api/products/64a141d3821cd55198345157');
+    expect(response.statusCode).toBe(404);
 })
